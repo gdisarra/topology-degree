8b113c0c8a4e54c13f2525930f3100dc9820909e
gdisarra <56912925+gdisarra@users.noreply.github.com>
2025-06-03 16:11:30 +0200

    Add files via upload

--- gamma_utils.py
+++ gamma_utils.py
@@ -15,8 +15,8 @@ import copy
 from ripser import ripser
 import time
 #import pdb
-#plt.rcParams['font.family'] = 'serif'
-#plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
+plt.rcParams['font.family'] = 'serif'
+plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
 
 class Synthetic_Data:
 
